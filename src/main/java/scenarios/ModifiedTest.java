dffcc6b6478611ae2c38f3945eda403f728f6948
ArpanaM <muriki.arpana@gmail.com>
2024-06-18 06:23:23 +0530

    new script added

---
+++ src/main/java/scenarios/ModifiedTest.java
@@ -0,0 +1,12 @@
+package scenarios;
+
+import org.testng.annotations.Test;
+
+public class ModifiedTest {
+
+	@Test
+	public void testcase1()
+	{
+		System.out.println("this is my new test case created...");
+	}
+}
